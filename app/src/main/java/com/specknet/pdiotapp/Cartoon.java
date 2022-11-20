5adc98ca00885836d79578d41861db1edb31ea2a
gaopeicheng <peichengko@sina.com>
2022-11-20 18:42:44 +0000

    update UI

---
+++ app/src/main/java/com/specknet/pdiotapp/Cartoon.java
@@ -0,0 +1,40 @@
+package com.specknet.pdiotapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class Cartoon extends AppCompatActivity {
+
+
+    TextView appname;
+    LottieAnimationView lottie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cartoon);
+
+        appname = findViewById(R.id.appname);
+        lottie  = findViewById(R.id.lottie);
+
+
+        appname.animate().translationY(-1400).setDuration(2700).setStartDelay(0);
+        lottie.animate().translationY(2000).setDuration(2000).setStartDelay(2900);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        },4000);
+
+    }
+}
